23e68dc9240817861809a373372ce6c9c5fb5749
sameltanvi <33136682+sameltanvi@users.noreply.github.com>
2017-11-17 22:09:18 +0000

    Add files via upload

---
+++ employee-producer/src/main/java/com/javainuse/controllers/ResponseUpgrade.java
@@ -0,0 +1,39 @@
+package com.javainuse.controllers;
+
+public class ResponseUpgrade {
+private int Stock_price;
+private String Company_Name;
+private String Buyer;
+private String Seller;
+private int Number_of_shares;
+public int getStock_price() {
+	return Stock_price;
+}
+public void setStock_price(int stock_price) {
+	Stock_price = stock_price;
+}
+public String getCompany_Name() {
+	return Company_Name;
+}
+public void setCompany_Name(String company_Name) {
+	Company_Name = company_Name;
+}
+public String getBuyer() {
+	return Buyer;
+}
+public void setBuyer(String buyer) {
+	Buyer = buyer;
+}
+public String getSeller() {
+	return Seller;
+}
+public void setSeller(String seller) {
+	Seller = seller;
+}
+public int getNumber_of_shares() {
+	return Number_of_shares;
+}
+public void setNumber_of_shares(int number_of_shares) {
+	Number_of_shares = number_of_shares;
+}
+}
